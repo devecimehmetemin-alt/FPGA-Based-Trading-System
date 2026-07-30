248dfd69e1931408fef40a056c5e6cb5462ed81c
Mehmet Deveci <devecimehmetemin@gmail.com>
2026-07-30 19:12:03 +0100

    additions to async fifo

--- rtl/common/axis_cdc_fifo.sv
+++ rtl/common/axis_cdc_fifo.sv
@@ -5,8 +5,66 @@ module axis_cdc_fifo #(parameter int ADDR_W = 1000, parameter int PAYLOAD_W=10)(
     output [31:0] rd_data
 );
 
+logic [ADDR_W-1:0] mem [31:0];
+logic g_rptr, empty, b_rptr, g_wptr_sync; //rptr signals
+logic g_rptr_sync, g_wptr, b_wptr, full; //wptr signals
+
+always_ff @(posedge rd_clk) begin 
+    if (rd_reset) begin
+        empty <= 1;
+    end
+    else if (~empty && rd_valid) begin
+        rd_data <= mem[31:0][b_rptr];
+    end
+end
+
+always_ff @(posedge wr_clk) begin
+    if (wr_reset) begin
+        overflow <= 0;
+    end
+    else if (~full && wr_valid) begin
+        mem[31:0][b_wptr] <= wr_data;
+    end
+end
+
+rptr_handler u_rptr_handler(
+    .rd_clk, 
+    .rrst_n(rd_reset), 
+    .r_en(rd_ready & rd_valid),
+    .g_wptr_sync(g_wptr_sync)
+    .g_rptr,
+    .b_rptr,
+    .empty
+)
+
+wptr_handler u_wptr_handler(
+    .wr_clk, 
+    .wrst_n(wr_reset), 
+    .w_en(wr_valid),
+    .g_rptr_sync,
+    .g_wptr,
+    .b_wptr,
+    .full
+)
+
+synchroniser synchroniser_rd_to_wr( 
+    .in(g_wptr),
+    .clk(wr_clk),
+    .reset(wr_reset)
+    .synced(g_wptr_sync)
+);
+
+synchroniser synchroniser_wr_to_rd(
+    .in(g_rptr),
+    .clk(rd_clk),
+    .reset(rd_reset),
+    .synced(g_rptr_sync)
+);
+
 
 endmodule
+
+
 
 module rptr_handler(
     input rd_clk, rrst_n, r_en
@@ -67,7 +125,7 @@ module wptr_handler(
 endmodule
 
 module synchroniser(
-    input unsync, clk, reset,
+    input in, clk, reset,
     output synced
 );
     logic d1;
@@ -77,7 +135,7 @@ module synchroniser(
             d1 <= 0;
             synced <= 0;
         end else begin
-            d1 <= unsync;
+            d1 <= in;
             synced <= d1;
         end
     end
